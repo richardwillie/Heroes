85431ee705f951b77fa0a571437322bf7549d08f
richard willie <richardwillie.w@gmail.com>
2018-05-23 18:41:45 +0300

    add route

---
+++ src/main/java/Squad.java
@@ -0,0 +1,23 @@
+public class Squad {
+    private String squadName;
+    private String squadCause;
+    private int squadsize;
+
+  
+    public Squad(String name,String cause, int size) {
+      squadName = name;
+      squadCause = cause;
+      squadsize = size;
+
+    }
+  
+    public String getName() {
+      return squadName;
+    }
+    public String getCause() {
+        return squadName;
+      }
+      public String getSize() {
+        return squadName;
+      }
+  }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -2,5 +2,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SquadTest {
+    @Test
+    public void Squad_instantiatesCorrectly_true() {
+      Squad mySquad = new Squad("Mow the lawn");
+      assertEquals(true, mySquadinstanceof Squad);
+    }
+  
 
 }
